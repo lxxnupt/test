b5eb99ab23fc187f4d39c27172a6de9544befb7c
陆徐行 <lxx48592@ly.com>
2017-07-17 20:59:54 +0800

    22

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,7 +2,7 @@ package test;
 
 public class Test {
 	public void add(){
-		System.out.print("增加");
+		System.out.print("增加22");
 	}
 	public static void main(String[] args) {
 		System.out.print("第2版本开发完毕！需要合并");
